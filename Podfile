ec44643272385393f45446ee2c636b78ac2f36c0
gryphonyang <gryphonyang@tencent.com>
2019-12-20 17:56:13 +0800

    add pods

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '11.0'
+
+target 'JHContactTrue' do
+pod 'AFNetworking'
+pod 'Masonry'
+end
---
+++ Pods/Target Support Files/Pods-JHContactTrue/Pods-JHContactTrue-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JHContactTrue : NSObject
+@end
+@implementation PodsDummy_Pods_JHContactTrue
+@end
